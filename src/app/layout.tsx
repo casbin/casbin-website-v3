6183a1f2c83222df4ffcac8e9870138a559c9098
tjyyy3 <istjy@foxmail.com>
2026-01-27 13:34:57 +0800

    feat: update site favicon to casbin logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/images/favicon.png" type="image/png" />
         <link rel="preconnect" href="https://cdn.casbin.org" crossOrigin="anonymous" />
         <link rel="preload" as="video" href="https://cdn.casbin.org/video/background.mp4" type="video/mp4" />
       </head>
